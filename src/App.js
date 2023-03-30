40617c9df0e4cf46c20ad04344ef0f5b87d50ecd
MrSharipov <84614549+MrSharipov@users.noreply.github.com>
2023-03-30 11:18:55 +0500

    P is added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import { MyRandomQuote } from './MyRandomQuote';
+import "./App.css";
+import { MyRandomQuote } from "./MyRandomQuote";
 // import MyWeather from './MyWeather';
 // import MovieApp from './MovieApp';
 // import RandomQuote from './RandomQuote';
@@ -8,8 +8,9 @@ import { MyRandomQuote } from './MyRandomQuote';
 function App() {
   return (
     <div className="App">
-    <MyRandomQuote />
-    <h1>Salom</h1>
+      <MyRandomQuote />
+      <h1>Salom</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ad!</p>
     </div>
   );
 }
